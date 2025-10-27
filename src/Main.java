767b77ac864ce6278d93300af642d54d2b04f753
jeppehcs1 <s235223@dtu.dk>
2025-10-27 16:46:49 +0100

    bug

--- src/Main.java
+++ src/Main.java
@@ -28,7 +28,8 @@ public class Main {
                     handleIType(instr);
                     break;
                 case 0b0110011:
-
+                    handleRType(instr);
+                    break;
                 default:
                     System.out.println("Opcode " + opcode + " not yet implemented");
                     break;
@@ -46,6 +47,45 @@ public class Main {
 
         System.out.println("Program exit");
 
+    }
+
+    private static void handleRType(int instr) {
+        int rd = (instr >> 7) & 0x01f;
+        int funct3 = (instr >> 12) & 0x01f;
+        int rs1 = (instr >> 15) & 0x01f;
+        int rs2 = (instr >> 20) & 0x01f;
+        int funct7 = (instr >> 25) & 0x01f;
+        switch (funct3) {
+            case 0x0:
+                reg[rd] = (funct7 == 0x00) ? reg[rs1] + reg[rs2]: reg[rs1] - reg[rs2];
+                break;
+            case 0x4:
+                reg[rd] = reg[rs1] ^ reg[rs2];
+                break;
+            case 0x6:
+                reg[rs1] = reg[rs1] | reg[rs2];
+                break;
+            case 0x7:
+                reg[rs1] = reg[rs1] & reg[rs2];
+                break;
+            case 0x1:
+                reg[rs1] = reg[rs1] << reg[rs2];
+                break;
+            case 0x5:
+                System.out.println("TODO");
+                reg[rs1] = reg[rs1] << reg[rs2];
+                break;
+            case 0x2:
+                reg[rs1] = (rs1 < rs2) ? 1 : 0;
+                break;
+            case 0x3:
+                System.out.println("TODO");
+                reg[rs1] = (rs1 < rs2) ? 1 : 0;
+                break;
+            default:
+                System.out.println("Funct3 for R type " + funct3 + " not yet implemented");
+                break;
+        }
     }
 
     private static void handleIType(int instr) {
@@ -81,7 +121,7 @@ public class Main {
                 reg[rs1] = (rs1 < imm)?1:0;
                 break;
             default:
-                System.out.println("Funct3 " + funct3 + " not yet implemented");
+                System.out.println("Funct3 for I type" + funct3 + " not yet implemented");
                 break;
         }
 
