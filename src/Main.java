222ccc79c431a19e43157f9aa11df4c23e68e1fe
jeppehcs1 <s235223@dtu.dk>
2025-10-27 16:26:42 +0100

    god start

--- src/Main.java
+++ src/Main.java
@@ -20,15 +20,15 @@ public class Main {
 
             int instr = progr[pc >> 2];
             int opcode = instr & 0x7f;
-            int rd = (instr >> 7) & 0x01f;
-            int rs1 = (instr >> 15) & 0x01f;
-            int imm = (instr >> 20);
+
 
             switch (opcode) {
 
-                case 0x13:
-                    reg[rd] = reg[rs1] + imm;
+                case 0b0010011:
+                    handleIType(instr);
                     break;
+                case 0b0110011:
+
                 default:
                     System.out.println("Opcode " + opcode + " not yet implemented");
                     break;
@@ -45,6 +45,45 @@ public class Main {
         }
 
         System.out.println("Program exit");
+
+    }
+
+    private static void handleIType(int instr) {
+        int rd = (instr >> 7) & 0x01f;
+        int rs1 = (instr >> 15) & 0x01f;
+        int funct3 = (instr >> 12) & 0x01f;
+        int imm = (instr >> 20);
+
+        switch (funct3) {
+            case 0x0:
+                reg[rd] = reg[rs1] + imm;
+                break;
+            case 0x4:
+                reg[rd] = reg[rs1] ^ imm;
+                break;
+            case 0x6:
+                reg[rs1] = reg[rs1] | imm;
+                break;
+            case 0x7:
+                reg[rs1] = reg[rs1] & imm;
+                break;
+            case 0x1:
+                System.out.println("TODO");
+                break;
+            case 0x5:
+                System.out.println("TODO");
+                break;
+            case 0x2:
+                reg[rs1] = (rs1 < imm)?1:0;
+                break;
+            case 0x3:
+                System.out.println("TODO");
+                reg[rs1] = (rs1 < imm)?1:0;
+                break;
+            default:
+                System.out.println("Funct3 " + funct3 + " not yet implemented");
+                break;
+        }
 
     }
 
